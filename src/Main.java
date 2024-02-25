9d99bc7f5220126c0f7f47f12d12ccc3432dbad5
Sertaç Kıncal <sertackincal@hotmail.com>
2024-02-25 15:13:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
         int number ,total =0;
         int digitCount;
-        System.out.println("Enter a number : ");
+        System.out.println("Enter a number : "); //Bir sayı al
         number = input.nextInt();
 
-        while (number != 0){
-            digitCount = number % 10;
-            number /=10;
+        while (number != 0){     //Girilen sayi 0 olana kadar döngü devam eder.
+            digitCount = number % 10; // Sayinin son basamak sayısını alır
+            number /=10;  //  Sayinin son basamak sayısını azaltır
 
-            total += digitCount;
+            total += digitCount; Son basamk rakamını bir önceki basamak sayısıyla toplar
 
         }
         System.out.println("Total digitcounter : " + total);
